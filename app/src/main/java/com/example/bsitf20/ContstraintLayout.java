f86830e46d55b8ea872ff2d6dcdc28da7151d978
Waheed <waheed.sep@gmail.com>
2023-10-23 14:57:33 +0500

    October 23

--- app/src/main/java/com/example/bsitf20/ContstraintLayout.java
+++ app/src/main/java/com/example/bsitf20/ContstraintLayout.java
@@ -6,16 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ContstraintLayout extends AppCompatActivity {
     Button btnGoback;
+    TextView tvName, tvPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contstraint_layout);
 
+        tvName = findViewById(R.id.tvName);
+        tvPassword = findViewById(R.id.tvPassword);
         btnGoback = findViewById(R.id.btnGoBack);
+
+        Intent recIntent = getIntent();
+        tvName.setText(recIntent.getStringExtra("name"));
+        tvPassword.setText(recIntent.getStringExtra("password"));
+
         btnGoback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/bsitf20/MainActivity.java
+++ app/src/main/java/com/example/bsitf20/MainActivity.java
@@ -1,26 +1,26 @@
 package com.example.bsitf20;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.text.TextUtils;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnLogin, btnGoto;
-    EditText edtEmail, edtPassword;
+    EditText edtName, edtPassword;
     String email, password;
     TextView tvLogin;
 
@@ -28,52 +28,88 @@ public class MainActivity extends AppCompatActivity {
     ImageView imgFlag;
 
     @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.context_menu, menu);
+        menu.setHeaderIcon(R.drawable.pakistan);
+        menu.setHeaderTitle("Select Menu");
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Handle item selection
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        btnLogin = findViewById(R.id.btnLogin);
         btnGoto = findViewById(R.id.btnGoto);
-//        edtEmail = findViewById(R.id.edtEmail);
-//        edtPassword = findViewById(R.id.edtPassword);
-        tvLogin = findViewById(R.id.tvLogin);
+        edtName = findViewById(R.id.edtName);
+        edtPassword = findViewById(R.id.edtPassword);
+//        tvLogin = findViewById(R.id.tvLogin);
         imgFlag = findViewById(R.id.imgFlag);
+
+        registerForContextMenu(btnGoto);
 
         btnGoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent secondActivity = new Intent(MainActivity.this, ContstraintLayout.class);
-                Intent gotoGoogle = new Intent(Intent.ACTION_VIEW);
-                gotoGoogle.setData(Uri.parse("033398471122"));
-                startActivity(gotoGoogle);
 
+//                String name, password;
+//                name = edtName.getText().toString();
+//                password = edtPassword.getText().toString();
+//                Intent secondActivity = new Intent(MainActivity.this, ContstraintLayout.class);
+//                if (TextUtils.isEmpty(edtName.getText())) {
+//                    edtName.setError("Name field cannot be blank");
+//                } else if (TextUtils.isEmpty(edtPassword.getText())) {
+//                    edtPassword.setError("Password field cannot be blank");
+//                } else {
+//                    secondActivity.putExtra("name", name);
+//                    secondActivity.putExtra("password", password);
+//                Intent gotoGoogle = new Intent(Intent.ACTION_VIEW);
+//                gotoGoogle.setData(Uri.parse("www.google.com.pk"));
+//                    startActivity(secondActivity);
+//                }
+//
+//
             }
         });
 
-        lvStudents = findViewById(R.id.lvStudents);
-        int[] flags = {R.drawable.afghanistan, R.drawable.canada, R.drawable.pakistan, R.drawable.china, R.drawable.iran};
+//        lvStudents = findViewById(R.id.lvStudents);
+//        int[] flags = {R.drawable.afghanistan, R.drawable.canada, R.drawable.pakistan, R.drawable.china, R.drawable.iran};
 //        String[] arrStudents = {"Laraib", "Amna", "Kainat", "Iqra", "Sania", "Sakina", "Tayyaba", "Aiman", "Afshan", "Mahpara"};
         ArrayAdapter<String> adpStudents = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, getResources().getStringArray(R.array.countries_ur));
-        lvStudents.setAdapter(adpStudents);
-        lvStudents.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                Toast.makeText(MainActivity.this, adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
-//                if (i == 0) {
-//                    imgFlag.setImageResource(R.drawable.pakistan);
-//                } else if ()
-
-                switch (i) {
-                    case 0:
-                        imgFlag.setImageResource(R.drawable.pakistan);
-                        break;
-                    case 1:
-                        imgFlag.setImageResource(R.drawable.afghanistan);
-                        break;
-                    case 2:
-                        imgFlag.setImageResource(R.drawable.iran);
-                }
-            }
-        });
+//        lvStudents.setAdapter(adpStudents);
+//        lvStudents.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+////                Toast.makeText(MainActivity.this, adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
+////                if (i == 0) {
+////                    imgFlag.setImageResource(R.drawable.pakistan);
+////                } else if ()
+//
+//                switch (i) {
+//                    case 0:
+//                        imgFlag.setImageResource(R.drawable.pakistan);
+//                        break;
+//                    case 1:
+//                        imgFlag.setImageResource(R.drawable.afghanistan);
+//                        break;
+//                    case 2:
+//                        imgFlag.setImageResource(R.drawable.iran);
+//                }
+//            }
+//        });
 //        edtEmail.addTextChangedListener(new TextWatcher() {
 //            @Override
 //            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
